df26da32daf11f3aebf54370c6b8581f3c9fd3c7
dhaval1997 <drprajapati000@gmail.com>
2023-10-04 10:33:13 +0530

    added delete expene button with event listner

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,8 +1,17 @@
 import "./ExpenseItem.css";
+import "./ExpenseDate.css";
 
 const ExpenseItem = ({ date, title, price, location }) => {
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear(); // Extract the year from the date
+
+  const handleDeleteExpense = () => {
+    const expenseItem = document.querySelector(".expense-item__description");
+    if (expenseItem) {
+      expenseItem.closest(".expense-item").remove();
+    }
+  };
 
   return (
     <>
@@ -10,12 +19,16 @@ const ExpenseItem = ({ date, title, price, location }) => {
         <div className="expense-date">
           <div className="expense-date__month">{month}</div>
           <div className="expense-date__day">{day}</div>
+          <div className="expense-date__year">{year}</div> {/* Add year */}
         </div>
         <div className="expense-item__description">
           <h2>
             {title} {location}
           </h2>
           <div className="expense-item__price">{price}</div>
+          <button value={title} onClick={handleDeleteExpense}>
+            Delete Expense
+          </button>
         </div>
       </div>
     </>
